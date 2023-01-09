c2d3d47cd1c17614f43a5106ec7105d381fbd26d
Ara <60602858+arajo-hub@users.noreply.github.com>
2023-01-09 12:04:26 +0900

    문제풀이 개선

--- src/chapter3/Q062.java
+++ src/chapter3/Q062.java
@@ -6,45 +6,36 @@ import java.io.InputStreamReader;
 
 public class Q062 {
 
-    public static final int INF = 1 * 99 - 1; // 비용 * (노드 개수 - 1) - 1
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int nodeCnt = Integer.parseInt(br.readLine());
 
-        int[][] graph = new int[nodeCnt][nodeCnt];
+        boolean[][] graph = new boolean[nodeCnt][nodeCnt];
 
         for (int i = 0; i < graph.length; i++) {
             String[] s = br.readLine().split(" ");
             for (int j = 0; j < s.length; j++) {
-                if (s[j].equals("0")) {
-                    s[j] = INF + "";
-                }
-                graph[i][j] = Integer.parseInt(s[j]);
+                graph[i][j] = s[j].equals("1");
             }
         }
 
         for (int i = 0; i < nodeCnt; i++) { // 거쳐갈 노드 선택
             for (int j = 0; j < nodeCnt; j++) {
                 for (int k = 0; k < nodeCnt; k++) {
-                    graph[j][k] = Math.min(graph[j][k], graph[j][i] + graph[i][k]);
+                    graph[j][k] = graph[j][k] || (graph[j][i] && graph[i][k]);
                 }
             }
         }
 
         for (int i = 0; i < graph.length; i++) {
             for (int j = 0; j < graph[i].length; j++) {
-                if (graph[i][j] == INF) {
-                    graph[i][j] = 0;
-                }
-                if (graph[i][j] > 0) {
-                    graph[i][j] = 1;
-                }
-                System.out.print(graph[i][j] + " ");
+                System.out.print(graph[i][j] ? "1 " : "0 ");
             }
             System.out.println();
         }
+
+        br.close();
     }
 
 }
