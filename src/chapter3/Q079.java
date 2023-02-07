08ae04748acc8ba68f2745fa7eeeda2c48b5a35f
Ara Jo <joara9566@naver.com>
2023-02-07 22:24:09 +0900

    문제풀이 추가

---
+++ src/chapter3/Q079.java
@@ -0,0 +1,36 @@
+package chapter3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q079 {
+
+    private static int[][] dp = new int[30][30];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            System.out.println(combination(m, n));
+        }
+    }
+
+    private static int combination(int m, int n) {
+        if (dp[m][n] > 0) {
+            return dp[m][n];
+        }
+        if (m == n || n == 0) {
+            return dp[m][n] = 1;
+        }
+        return dp[m][n] = combination(m - 1, n - 1) + combination(m - 1, n);
+    }
+
+}
