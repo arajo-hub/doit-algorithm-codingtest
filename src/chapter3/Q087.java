052759a0aa83f072e0bd3d8880f75fa5cd7db314
Ara <60602858+arajo-hub@users.noreply.github.com>
2023-02-21 11:37:47 +0900

    문제풀이 추가

---
+++ src/chapter3/Q087.java
@@ -0,0 +1,33 @@
+package chapter3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q087 {
+
+    private static long[] d;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        d = new long[n + 1];
+
+        d[0] = 0;
+        d[1] = 1;
+
+        if (n >= 2) {
+            d[2] = 2;
+        }
+
+        for (int i = 3; i <= n; i++) {
+            d[i] = (d[i - 2] + d[i - 1]) % 10007;
+        }
+
+        System.out.println(d[n] % 10007);
+    }
+}
